eabe05eedad344d133088d849fb7aa04c1f96502
b1mounaim <36512297+b1mounaim@users.noreply.github.com>
2018-04-28 23:02:49 +0100

    Add files via upload

---
+++ includes/layout/footer.php
@@ -0,0 +1,8 @@
+
+    <script src="public/static/jquery/jquery-3.1.1.slim.min.js"></script>
+    <script src="public/static/tether/tether.min.js"></script>
+    <script src="public/static/js/bootstrap.min.js" ></script>
+
+<?php mysqli_close($conn);?>
+    </body>
+</html>
---
+++ includes/layout/header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Mounaim</title>
+    
+    <link rel="stylesheet" href="public/static/css/bootstrap.min.css">
+    
+          
+    <href="public/static/awesome-4.7.0/css/font-awesome.min.css">
+  </head>
+  
